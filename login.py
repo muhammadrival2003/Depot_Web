0b1c3f232663886626566adebd5baf4690a5b177
muhammadrival2003 <muhammadrival040303@gmail.com>
2022-09-11 09:57:00 +0700

    Codingan awal login

---
+++ login.py
@@ -0,0 +1,11 @@
+nama = input("Masukkan nama anda: ")
+password = input("Masukkan password anda: ")
+
+Nama = "rival"
+Password = "2003"
+
+if (nama == Nama):
+    if (password == Password):
+        {
+            print("Anda berhasil LOgin")
+        }
