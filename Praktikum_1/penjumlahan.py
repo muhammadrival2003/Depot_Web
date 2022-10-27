d596546fd9d8c7dfe588754d43987f33bab5cf1a
muhammadrival2003 <muhammadrival040303@gmail.com>
2022-10-28 00:14:50 +0700

    Ini terbaru

--- Praktikum_1/penjumlahan.py
+++ Praktikum_1/penjumlahan.py
@@ -4,6 +4,12 @@ c = a * b
 
 print("Hasil penjumlahannya adalah ", c)
 
+a = 10
+b = 56
+c = a * b
+
+print("Hasil = ", c)
+
 nama = input("Masukkan nama anda: ")
 password = input("Masukkan password anda: ")
 
