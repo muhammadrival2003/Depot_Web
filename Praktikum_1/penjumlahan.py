9bdb2f3a0005f0704cb2b347c96bac8f6cc3baa1
muhammadrival2003 <muhammadrival040303@gmail.com>
2022-10-28 00:29:56 +0700

    Update penjumlahan.py

--- Praktikum_1/penjumlahan.py
+++ Praktikum_1/penjumlahan.py
@@ -4,12 +4,13 @@ c = a * b
 
 print("Hasil penjumlahannya adalah ", c)
 
-a = 10
-b = 56
-c = a * b
-
-print("Hasil = ", c)
-
-nama = input("Masukkan nama anda: ")
-password = input("Masukkan password anda: ")
-
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
+#asdfjasdjlfashdfashfalsdfj
